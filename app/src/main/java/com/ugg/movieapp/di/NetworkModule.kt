dc7785f51a6eea80660b8a944e7a89a35c8d1f35
UGG Macbook <>
2022-08-24 14:44:37 +0300

    Added client to retrofit

--- app/src/main/java/com/ugg/movieapp/di/NetworkModule.kt
+++ app/src/main/java/com/ugg/movieapp/di/NetworkModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,10 +17,24 @@ import javax.inject.Singleton
 object NetworkModule {
     @Singleton
     @Provides
-    fun provideMovieAPI() : MovieAPI {
+    fun provideMovieAPI(
+        okHttpClient: OkHttpClient
+    ) : MovieAPI {
         return  Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(MovieAPI::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideHTTPClient() : OkHttpClient{
+        return OkHttpClient.Builder()
+            .apply {
+                addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+            }.build()
     }
 }
